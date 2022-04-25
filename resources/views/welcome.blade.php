cb3451d4d3a9402995e2ed901fbed67a82a80ba0
Quan Le Van <quanbka.cntt@gmail.com>
2022-04-25 18:51:15 +0700

    %

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,115 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title></title>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+            <div id="container" style="width:100%; height:400px;"></div>
+            <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <script type="text/javascript">
+            chart = Highcharts.chart('container', {
+                title: {
+                    text: 'Tỉ lệ autofulfill trong 7 ngày vừa qua'
+                },
+                subtitle: {
+                    text: 'Nguồn: Printerval'
+                },
+                xAxis: {
+                    categories: [
+                        'Jan',
+                        'Feb',
+                        'Mar',
+                        'Apr',
+                        'May',
+                        'Jun',
+                        'Jul'
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+                    ],
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Tỉ lệ auto fulfill (mm)'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Tỉ lệ auto fulfill',
+                    data: []
+
+                }]
+            });
+
+            async function initSlide () {
+                let days = await get7RecentDays();
+                console.log(days);
+                let ratio = await getRatios(days);
+                console.log(ratio);
+
+            }
+
+            async function get7RecentDays () {
+                let retval = [];
+                for (var i = -90; i < 0; i++) {
+                    retval.push(new Date(Date.now() + i * 84600000).toISOString().slice(0, 10));
+                }
+                chart.update({
+                    xAxis : {
+                        categories : retval
+                    }
+                });
+                return retval;
+            }
+
+            async function getRatios (days) {
+                let retval = [];
+                for (var i = 0; i < days.length; i++) {
+                    retval.push(await getRatio(days[i]));
+                    chart.update({
+                        series : {
+                            data : retval
+                        }
+                    });
+                }
+
+                return retval;
+            }
+
+            async function getRatio (day) {
+                let url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count`;
+                let response = await axios.get(url);
+                let order = response.data.result;
+                console.log(order);
+                url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count&scopes=orderMeta(keys=[is_auto_fulfill];values=[4])`;
+                response = await axios.get(url);
+                let fulfill_order = response.data.result;
+                console.log(fulfill_order);
+                return fulfill_order * 100 / order;
+            }
+
+            initSlide()
+
+            </script>
+
+
+
+            Filter
     </body>
 </html>
