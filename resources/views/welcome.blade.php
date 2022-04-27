db403585aafc5e1ea55c50a1ee3ff05b5962d9a6
Quan Le Van <quanbka.cntt@gmail.com>
2022-04-27 09:58:31 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,26 @@
         <title></title>
     </head>
     <body>
-            <div id="container" style="width:100%; height:400px;"></div>
+            <div id="container" style="width:100%; height:800px;"></div>
             <script src="https://code.highcharts.com/highcharts.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
             <script type="text/javascript">
             chart = Highcharts.chart('container', {
+                chart: {
+                    type: 'area'
+                },
+                plotOptions: {
+                    area: {
+                        stacking: 'normal',
+                        lineColor: '#666666',
+                        lineWidth: 1,
+                        marker: {
+                            lineWidth: 1,
+                            lineColor: '#666666'
+                        }
+                    }
+                },
                 title: {
                     text: 'Tỉ lệ autofulfill trong 90 ngày vừa qua'
                 },
@@ -20,8 +34,14 @@
                 series: [{
                     name: 'Tỉ lệ auto fulfill',
                     data: []
-
-                }]
+                }, {
+                    name: 'Tỉ lệ không có template fulfill',
+                    data: []
+                }, {
+                    name: 'Tỉ lệ không có design',
+                    data: []
+                }
+            ]
             });
             var totalOrders = 0;
             var totalFulfillOrders = 0;
@@ -44,12 +64,30 @@
             }
             async function getRatios (days) {
                 let retval = [];
+                let retval2 = [];
+                let retval3 = [];
                 for (var i = 0; i < days.length; i++) {
-                    retval.push(await getRatio(days[i]));
+                    let ratio = await getRatio(days[i]);
+                    retval.push(ratio.fulfill * 100 / ratio.order);
+                    retval2.push(ratio.notTemplate * 100 / ratio.order);
+                    retval3.push(ratio.notDesign * 100 / ratio.order);
+
                     chart.update({
-                        series : {
-                            data : retval
-                        },
+                        series : [
+
+                            {
+                                name : "Tỉ lệ đơn không có template",
+                                data : retval2
+                            },
+                            {
+                                name : "Tỉ lệ đơn không có design",
+                                data : retval3
+                            },
+                            {
+                                name : "Tỉ lệ auto fulfill",
+                                data : retval
+                            },
+                        ],
                         subtitle : { text : `Số đơn autofulfill: ${totalFulfillOrders} / ${totalOrders} ` },
                         title : { text : `Tỉ lệ auto fulfill : ${totalFulfillOrders * 100 / totalOrders} %` },
                     });
@@ -60,12 +98,27 @@
                 let url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count`;
                 let response = await axios.get(url);
                 let order = response.data.result;
-                totalOrders += order;
+
                 url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count&scopes=orderMeta(keys=[is_auto_fulfill];values=[4])`;
                 response = await axios.get(url);
-                let fulfill_order = response.data.result;
-                totalFulfillOrders += fulfill_order;
-                return fulfill_order * 100 / order;
+                let fulfill = response.data.result;
+
+                url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count&scopes=orderMeta(keys=[reason_auto_fulfill_fail,is_auto_fulfill];values=[template,0])`;
+                response = await axios.get(url);
+                let notTemplate = response.data.result;
+
+                url = `https://glob.api.printerval.com/v2/order?sorts=-created_at&get_is_merge=1&filters=order.created_at=[${day};${day}%2023:59:59],order.payment_status=paid&metric=count&scopes=orderMeta(keys=[reason_auto_fulfill_fail,is_auto_fulfill];values=[design,2])`;
+                response = await axios.get(url);
+                let notDesign = response.data.result;
+
+                totalOrders += order;
+                totalFulfillOrders += fulfill;
+                return {
+                    fulfill,
+                    order,
+                    notTemplate,
+                    notDesign
+                };
             }
             initSlide()
             </script>
