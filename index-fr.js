e9091e8d58052f8cc8e64331bdb6beaaefae0d17
Quan Le Van <quanbka.cntt@gmail.com>
2022-05-19 15:59:21 +0700

    ok

---
+++ index-fr.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+const axios = require('axios').default;
+
+(async () => {
+
+let json = fs.readFileSync('fr-gelato.json');
+let jsonData = JSON.parse(json);
+// console.log(response.result);
+for (var i = 0; i < jsonData.result.length; i++) {
+    let order = jsonData.result[i];
+    let data = JSON.parse(order.data);
+    // console.log(order.target_id);
+    let items = data.items;
+    // console.log(items);
+    let item = items[0];
+    let url = `https://fr.api.printerval.com/order_item/${items[0].itemReferenceId}`;
+    // console.log(url);
+    let response = await axios.get(url)
+    if (response.data.result) {
+        let product_sku_id = response.data.result.product_sku_id;
+        // console.log(product_sku_id);
+        url = `https://printerval.com/fr/print-suppliers/${product_sku_id}/template`;
+        response = await axios.get(url)
+        if (response.data.input[0]) {
+            console.log(response.data.input)
+            console.log(item.productUid);
+        }
+    }
+}
+
+})();
